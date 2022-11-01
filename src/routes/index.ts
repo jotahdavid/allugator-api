5a5f33bc2d29d1f8f4899813bd036c349cc4feed
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-11-01 09:32:51 -0300

    feat(user): route to create one

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
 import ProductController from '../app/controllers/ProductController';
+import UserController from '../app/controllers/UserController';
 
 const router = Router();
 
 router.get('/products', ProductController.index);
 router.post('/products', ProductController.store);
+
+router.post('/users', UserController.store);
 
 export default router;
