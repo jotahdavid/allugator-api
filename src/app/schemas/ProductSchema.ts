ce06b2c38975100f2561da5aa5c165dd9157fb9b
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-11-02 09:49:28 -0300

    fix(product): add min 1 character to description field

--- src/app/schemas/ProductSchema.ts
+++ src/app/schemas/ProductSchema.ts
@@ -4,5 +4,5 @@ export default z.object({
   name: z.string().min(1),
   price: z.number().min(1),
   imageUrl: z.string().url(),
-  description: z.string().optional(),
+  description: z.string().min(1).optional(),
 });
