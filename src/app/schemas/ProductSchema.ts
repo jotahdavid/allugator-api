75953a8e7682099b3ad2e9b71cd919ba0da643f4
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-11-03 12:59:37 -0300

    fix(product): add rent price field to schema

--- src/app/schemas/ProductSchema.ts
+++ src/app/schemas/ProductSchema.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 export default z.object({
   name: z.string().min(1),
   price: z.number().min(1),
+  rentPrice: z.number().min(1),
   imageUrl: z.string().url(),
   description: z.string().min(1).optional(),
 });
