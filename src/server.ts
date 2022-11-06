38570877beca21010d77fa9f4258ed0edca55c59
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-11-06 10:56:47 -0300

    docs: add API docs with swagger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './docs/swagger.json';
+
 import routes from './routes';
 
 const app = express();
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(cors());
 app.use(express.json());
 app.use(routes);
