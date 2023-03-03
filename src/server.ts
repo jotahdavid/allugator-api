3b0dd79265ec34ba974eb012561f019c6682218a
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2023-03-03 14:33:52 -0300

    fix: handle with database connection failure

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,33 @@
+/* eslint-disable no-console */
 import express from 'express';
 import cors from 'cors';
 
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocs from './docs/swagger.json';
 
+import prisma from '@services/prisma';
 import routes from './routes';
 
 const app = express();
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-app.use(cors());
-app.use(express.json());
-app.use(routes);
+async function bootstrap() {
+  try {
+    await prisma.$connect();
+    console.log('✅ Database connected successfully');
+  } catch {
+    console.error('⛔ Database connection failure');
+    return;
+  }
 
-const PORT = process.env.PORT ?? 3001;
-// eslint-disable-next-line no-console
-app.listen(PORT, () => console.log(`🔥 Server running at http://localhost:${PORT}`));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+  app.use(cors());
+  app.use(express.json());
+  app.use(routes);
+
+  const port = process.env.PORT ?? 3001;
+  app.listen(port, () => {
+    console.log(`🔥 Server running at http://localhost:${port}`);
+  });
+}
+
+bootstrap();
